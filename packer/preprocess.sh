73f2dc8f6761b078af80cea896b7addc41e94441
fabs <fsalaman@google.com>
2024-02-14 12:17:48 -0600

    Template version

--- packer/preprocess.sh
+++ packer/preprocess.sh
@@ -5,14 +5,14 @@
 # Fabian Salamanca <fsalaman@google.com>
 # ******************************************************
 
-REPO="https://github.com/fabssaldom/demobakeryansible"
+REPO="<Repo for Ansible playbook>" # Use a git repo for Ansible playbooks
 WDIR="/tmp"
 
 sudo dnf -y upgrade
 sudo dnf config-manager --enable "rhui-rhel-9-for-x86_64-appstream-rhui-rpms"
 sudo dnf -y install git python3-pip ansible-core
 FDIR=${WDIR}/demobakery
-git clone ${REPO} ${FDIR}
+git clone ${REPO} ${FDIR} # Recommended add SSH keys for Github private repos
 cd ${FDIR}
 ansible-playbook -i hosts linux.yaml
 cd ${WDIR}
